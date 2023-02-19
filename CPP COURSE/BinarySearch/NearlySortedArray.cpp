deb3102b66e54c41cd30814100d3eed44937f81c
Priyankawani <priyankawani14@gmail.com>
2023-02-19 12:34:19 +0530

    Update NearlySortedArray.cpp

--- CPP COURSE/BinarySearch/NearlySortedArray.cpp
+++ CPP COURSE/BinarySearch/NearlySortedArray.cpp
@@ -1,65 +1,45 @@
 #include <iostream>
-#include <cmath>
-#include <limits.h>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-void printnumsay(vector<int> nums)
+int binarySearch(vector<int>arr, int target)
 {
-    for (auto value : nums)
+  int s=0;
+  int e = arr.size()-1;
+  int mid = s+((e-s)/2);
+  while(s<=e)
     {
-        cout << value << " ";
+      
+      if(arr[mid]==target)
+      {
+        return mid;
+      }
+      if(mid-1>=0 && arr[mid-1]==target)
+      {
+        return mid-1;
+      }  
+      if(mid+1<arr.size() && arr[mid+1]==target)
+      {
+        return mid+1;
+      }
+      if(target>arr[mid])
+      {
+        s=mid+2;
+      }
+      else
+      {
+        e=mid-2;
+      }
+      mid = s+((e-s)/2);
     }
-    cout<<endl;
+  return -1;
 }
+int main() {
+  vector<int> arr{10,3,40,20,50,80,70};
+  int target = 80;
 
-int binarySearch(vector<int> arr, int target){
-    int start = 0; int end = arr.size(); int mid = (start+end)/2; 
+  int ans = binarySearch(arr,target);
+  cout<<"index of "<<target<<" is "<<ans<<endl;
 
-    while(start<=end){
-        if(arr[mid] == target){
-            return mid;
-        }
-        if(mid-1 >=0 && arr[mid-1] == target){
-            return mid-1;
-        }
-        if(mid+1<=arr.size() && arr[mid+1] == target){
-            return mid+1;
-        }
-        else if(target<arr[mid]){
-            //search left side
-            end = mid - 2;
-        }
-        else{
-            // search right side
-            start = mid + 2;
-        }
-
-        mid = (start + end)/2;
-    }
-    return -1;
-}
-
-int main()
-{
-    vector<int> arr{10,3,40,20,50,80,70};
-    int target = 50;
-    // sort(arr.begin(),arr.end());
-    int indexOfTarget = binarySearch(arr, target);
-    printnumsay(arr);
-
-    if(indexOfTarget == -1){
-        cout<<"Not found"<<endl;
-    }
-    else{
-        cout<<"found, index value is: "<<indexOfTarget<<endl;
-    }
-    
-    
-
-
-    // printnumsay(arr);
-
-    return 0;
+  return 0;
 }

148ec2524e168c6cda82d081728c24ce4fbeace5
Priyankawani <priyankawani14@gmail.com>
2023-02-24 22:14:36 +0530

    Create BubbleSortBestCase.cpp

---
+++ CPP COURSE/BubbleSortBestCase.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+  vector<int> arr{10,1,7,6,14,9};
+  int n = arr.size();
+  bool swapped = false;
+  //Bubble sort
+  for(int round=0; round<n-1; round++)
+    {
+      for(int j=0;j< n-round-1; j++)
+        {
+          if(arr[j]>arr[j+1])
+          {
+            swapped = true;
+            swap(arr[j],arr[j+1]);
+          }
+        }
+      if(swapped==false)
+      {
+        break;
+      }
+    }
+  //print
+  for(int i=0; i<n; i++)
+    {
+      cout<<arr[i]<<" ";
+    }
+}
+
+
+
+

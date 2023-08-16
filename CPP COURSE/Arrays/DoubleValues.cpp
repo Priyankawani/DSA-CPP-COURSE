3ebee1ab252df42e312fa66e8f59b832596855c6
Priyankawani <priyankawani14@gmail.com>
2023-08-16 14:20:00 +0530

    Updated DoubleValues03

--- CPP COURSE/Arrays/DoubleValues.cpp
+++ CPP COURSE/Arrays/DoubleValues.cpp
@@ -1,44 +1,19 @@
 #include <iostream>
-#include<math.h>
 using namespace std;
-int solve(int dividend, int divisor)
-{
-  int s= 0;
-  int e = abs(dividend);
-  int ans=1;
-  int mid = s+((e-s)/2);
-  while(s<=e)
-    { 
-      if(abs(mid * divisor) == dividend)
-      {
-        ans = mid;
-        break;
-      }
-      if(abs(mid * divisor) > dividend)
-      {
-        e=mid-1;
-      }
-      else
-      {
-        ans = mid;
-        s=mid+1;
-      }
-      mid = s+((e-s)/2);
-    }
-  if((divisor<0 && dividend<0)||(divisor>0 && dividend>0))
-  {
-    return ans;
-  }
-  else
-  {
-    return -ans;
-  }
-}
-int main() {
-  int dividend = 35;
-  int divisor = -5;
 
-  int ans = solve(dividend, divisor);
-  cout<<"Ans is "<<ans<<endl;
-  
+int main() {
+  int arr[5];
+  cout << "Enter the array numbers: " << endl;
+  for (int i = 0; i < 5; i++)
+  {
+    cin >> arr[i];
+    cout << " ";
+  }
+
+  cout << "Printing doubles: " << endl;
+  for (int j = 0; j < 5; j++)
+  {
+    cout << arr[j] * 2 << " ";
+  }
+  return 0;
 }

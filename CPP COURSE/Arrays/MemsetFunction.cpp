62f41713b9a9c74f2a061364e88322efbddd8565
Priyankawani <priyankawani14@gmail.com>
2023-08-22 09:13:28 +0530

    Updated MemsetFunction

---
+++ CPP COURSE/Arrays/MemsetFunction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+
+    memset(arr,0,sizeof(arr));
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";
+        
+    }
+    cout<<endl;
+    return 0;
+}

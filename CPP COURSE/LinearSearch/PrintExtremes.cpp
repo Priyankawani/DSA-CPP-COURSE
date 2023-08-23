69bdc9752926bbce44066045c8619431144f8239
Priyankawani <priyankawani14@gmail.com>
2023-08-23 10:46:27 +0530

    PrintExtreme01.cpp

--- CPP COURSE/LinearSearch/PrintExtremes.cpp
+++ CPP COURSE/LinearSearch/PrintExtremes.cpp
@@ -3,24 +3,23 @@ using namespace std;
 
 int main()
 {
-    int arr[10] = {10,20,30,40,50,60,70,80,90,100};
-    int size = 10;
+    int arr[10] = {10,20,30,40,50,60,70,80,90};
+    int size = 9;
     int start = 0;
     int end = size-1;
-    for(int i=0; i<size;i++)
+   
+    while(start<=end)
     {
-        if(start<end)
-    {
-        
-        cout<<arr[start]<<" "<<arr[end]<<" ";
+        if(start==end)
+        {
+            cout<<arr[start]<<" ";
+        }
+        else
+        {
+            cout<<arr[start]<<" "<<arr[end]<<" ";
+        }
         start++;
         end--;
-    }
-    else if(start==end)
-    {
-        cout<<arr[end];
-        break;
-    }
     }
     
 }

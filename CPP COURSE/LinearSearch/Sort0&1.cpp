036c9c8b587d61f74b0fe191159bb05d4e72a274
Priyankawani <priyankawani14@gmail.com>
2023-09-07 23:32:44 +0530

    Sorting.cpp

---
+++ CPP COURSE/LinearSearch/Sort0&1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr {1,0,1,1,1,0,0,1,0,1};
+
+    int s = 0;
+    int e = arr.size()-1;
+    int i=0;
+
+    while(s<=e)
+    {
+        if(arr[i]==0)
+        {
+            swap(arr[i],arr[s]);
+            i++;
+            s++;
+        }
+        else{
+            swap(arr[i],arr[e]);
+            e--;
+        }
+    }
+
+    for(auto value : arr)
+    {
+        cout<<value<<" ";
+    }
+}

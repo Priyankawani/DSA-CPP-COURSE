a434c330f2c048cf1c0abd85bb6ef5d682ee182d
Priyankawani <priyankawani14@gmail.com>
2023-09-07 15:34:32 +0530

    unionwithoutDuplicates.cpp

--- CPP COURSE/LinearSearch/build/Unique&Intersection.cpp
+++ CPP COURSE/LinearSearch/build/Unique&Intersection.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<limits.h>
 using namespace std;
 
 void UnionArray(int a[], int sizea, int b[], int sizeb)
@@ -20,6 +21,40 @@ void UnionArray(int a[], int sizea, int b[], int sizeb)
         cout<<ans[i]<<" ";
     }
 
+}
+
+void UnionWithoutDuplicates(int arr[], int sizearr, int brr[], int sizebrr)
+{
+    vector<int> union1;
+    for(int i=0; i<sizearr; i++)
+    {
+        for(int j=0; j<sizebrr; j++)
+        {
+            if(arr[i] == brr[j])
+            {
+                //marking the element so that its not checked again
+                brr[j] = INT_MIN;
+            }
+        }
+    }
+
+    for(int i=0; i<sizearr; i++)
+    {
+        union1.push_back(arr[i]);
+    }
+    
+    for(int i=0; i<sizebrr; i++)
+    {
+        if(brr[i] != INT_MIN)
+        {
+            union1.push_back(brr[i]);
+        }
+    }
+
+    for(auto value: union1)
+    {
+        cout<<value<<" ";
+    }
 }
 
 void Intersection(int a[], int b[], int sizea, int sizeb)
@@ -44,13 +79,18 @@ void Intersection(int a[], int b[], int sizea, int sizeb)
 }
 int main()
 {
-    int a[] = {1,2,3,4,5,6};
-    int b[] = {4,5,6,7};
-    int sizea = 6;
-    int sizeb = 4;
+    // int a[] = {1,2,3,4,5,6};
+    // int b[] = {4,5,6,7};
+    // int sizea = 6;
+    // int sizeb = 4;
 
-    UnionArray(a, sizea, b, sizeb);
-    cout<<endl;
-    Intersection(a, b, sizea, sizeb);
+    // UnionArray(a, sizea, b, sizeb);
+    // cout<<endl;
+    // Intersection(a, b, sizea, sizeb);
 
+    int arr[] = {1,2,3,4,5,6,7};
+    int sizearr = 7;
+    int brr[] = {1,2,3,8,9,7,6};
+    int sizebrr = 7;
+    UnionWithoutDuplicates(arr, sizearr, brr, sizebrr);
 }
